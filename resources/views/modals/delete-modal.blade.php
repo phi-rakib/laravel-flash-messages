dd3a2973f9287758553cf5862c436c0a678d0ab2
rakib <phi.rakib@gmail.com>
2023-03-18 14:23:58 +0600

    added delete book modal

---
+++ resources/views/modals/delete-modal.blade.php
@@ -0,0 +1,23 @@
+<div class="modal" tabindex="-1" role="dialog" id="delete-modal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">
+                    Delete Confirmation
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            
+            <div class="modal-body">
+                <p>Are you sure to delete it</p>
+            </div>
+
+            <div class="modal-footer">
+                <a href="" class="btn btn-danger" id="delete-link">Delete</a>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
